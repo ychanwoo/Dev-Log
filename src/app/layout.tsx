626b59919b4a27a959cecbf9421c67577cbaef9b
ychanwoo <chxan02@naver.com>
2025-10-03 22:22:08 +0900

    chore: update DevLog page metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,28 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import FooterPage from './Footer/page'
-import HeaderPage from './Header/page'
+import type { Metadata } from "next";
+import "./globals.css";
+import FooterPage from "./Footer/page";
+import HeaderPage from "./Header/page";
 
 export const metadata: Metadata = {
-  title: 'My Blog',
-  description: '개인 블로그입니다.',
-}
+  title: "Dev Log",
+  description: "Dev Log 보관소 입니다.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="ko">
       <body className="flex flex-col min-h-screen">
         <HeaderPage />
-        <main className="flex-1">
-          {children}
-        </main>
+        <main className="flex-1">{children}</main>
         <FooterPage />
       </body>
     </html>
-  )
+  );
 }
