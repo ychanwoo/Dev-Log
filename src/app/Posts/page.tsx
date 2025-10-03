3be7a2a3844df657d00ed767addde88e5817538d
ychanwoo <chxan02@naver.com>
2025-10-03 22:24:54 +0900

    feat: render mdx files on DevLog page

--- src/app/Posts/page.tsx
+++ src/app/Posts/page.tsx
@@ -1,23 +1,65 @@
-import { Metadata } from "next";
-import Button from "../components/Button";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Link from "next/link";
+import Image from "next/image";
+import DevLogPageTagName from "../components/DevLogDetail";
+import { Post } from "../types/post";
+// 필터링 기능도 추가
+// 페이지네이션 기능 추가
 
-export const metadata: Metadata = {
-  title: 'Blog-List & New-Post',
-  description: 'Posts 페이지 입니다.'
-}
+export default function DevLogPage() {
+  // Posts 폴더 경로
+  const postDir = path.join(process.cwd(), "src/app/posts/posts-data");
 
-export default function PostsPage() {
+  // mdx 파일 리스트 읽기
+  const files = fs.readdirSync(postDir).filter((file) => file.endsWith(".mdx"));
+
+  // 파일 별로 fromtmatter 작성
+  const posts: Post[] = files.map((filename) => {
+    const filePath = path.join(postDir, filename);
+    const fileContent = fs.readFileSync(filePath, "utf-8");
+    const { data } = matter(fileContent);
+
+    return {
+      slug: filename.replace(".mdx", ""),
+      ...data,
+    } as Post;
+  });
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-[80vh] bg-white space-y-8">
-      
-      <h1 className="text-3xl font-bold text-blue-900">블로그 관리</h1>
-      <p className="text-gray-500 text-lg">원하는 작업을 선택하세요</p>
-
-      <div className="flex flex-col sm:flex-row sm:space-x-6 space-y-4 sm:space-y-0 mt-6">
-        <Button href="/Posts/List" className="bg-indigo-500 hover:bg-indigo-700 text-white px-8 py-4 text-lg font-semibold rounded-lg shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1">블로그 목록 보기</Button>
-        <Button href="/Posts/New" className="bg-green-500 hover:bg-green-700 text-white px-8 py-4 text-lg font-semibold rounded-lg shadow-lg hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1">새 블로그 작성</Button>
+    <div className="container mx-auto px-4 py-15">
+      {/* 한 페이지에 3x4 12개 씩 */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 mb-8">
+        {posts.map((post) => (
+          <div
+            key={post.slug}
+            className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow"
+          >
+            <Link href={`/posts/${(post as Post).slug}`} className="">
+              <div className="relative h-55">
+                <Image
+                  src={(post as Post).image}
+                  alt={`${(post as Post).title} Log thumbnail`}
+                  fill
+                  className="object-cover"
+                />
+              </div>
+              <div className="p-4">
+                <h2 className="text-lg font-semibold mb-2">
+                  {(post as Post).title}
+                </h2>
+                <div className="flex justify-between items-center">
+                  <span className="text-sm text-gray-500">
+                    {(post as Post).date}
+                  </span>
+                  <DevLogPageTagName post={post} />
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
       </div>
-
     </div>
   );
 }
