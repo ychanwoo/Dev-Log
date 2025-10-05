79a99f9a6b739199cf21a07111ccd639d8348473
ychanwoo <chxan02@naver.com>
2025-10-05 17:21:40 +0900

    feat: add filtering and page navigation

--- src/app/Posts/page.tsx
+++ src/app/Posts/page.tsx
@@ -5,10 +5,17 @@ import Link from "next/link";
 import Image from "next/image";
 import DevLogPageTagName from "../components/DevLogDetail";
 import { Post } from "../types/post";
-// 필터링 기능도 추가
-// 페이지네이션 기능 추가
+import Pagination from "../components/Pagination";
 
-export default function DevLogPage() {
+export default async function DevLogPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ page?: string; tag?: string }>;
+}) {
+  const params = await searchParams;
+  const currentPage = Number(params.page) || 1;
+  const postsPerPage = 9; // 한 페이지당 게시물 개수
+
   // Posts 폴더 경로
   const postDir = path.join(process.cwd(), "src/app/posts/posts-data");
 
@@ -16,7 +23,7 @@ export default function DevLogPage() {
   const files = fs.readdirSync(postDir).filter((file) => file.endsWith(".mdx"));
 
   // 파일 별로 fromtmatter 작성
-  const posts: Post[] = files.map((filename) => {
+  const allPosts: Post[] = files.map((filename) => {
     const filePath = path.join(postDir, filename);
     const fileContent = fs.readFileSync(filePath, "utf-8");
     const { data } = matter(fileContent);
@@ -27,10 +34,23 @@ export default function DevLogPage() {
     } as Post;
   });
 
+  // * filter 기능
+  const selectedTag = params.tag;
+  const filteredPosts = selectedTag
+    ? allPosts.filter((post) => post.tags?.includes(selectedTag))
+    : allPosts;
+
+  //* pagination (필터링된 결과에 적용)
+  const totalPosts = filteredPosts.length;
+  const totalPages = Math.ceil(totalPosts / postsPerPage);
+  const startIndex = (currentPage - 1) * postsPerPage;
+  const endIndex = startIndex + postsPerPage;
+  const posts = filteredPosts.slice(startIndex, endIndex);
+
   return (
     <div className="container mx-auto px-4 py-15">
-      {/* 한 페이지에 3x4 12개 씩 */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 mb-8">
+      {/* 한 페이지에 3x3 9개 씩 */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-10 mb-12">
         {posts.map((post) => (
           <div
             key={post.slug}
@@ -60,6 +80,7 @@ export default function DevLogPage() {
           </div>
         ))}
       </div>
+      <Pagination currentPage={currentPage} totalPage={totalPages} />
     </div>
   );
 }
